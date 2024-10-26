dea5e4fe09dce2a706bb266077873901afaffb76
Vovkri <kvs030030@gmail.com>
2024-10-26 13:00:25 +0400

    hot fix

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 
 import EditIcon from '../../assets/edit.svg?react';
+import SearchIcon from '../../assets/add.svg?react';
 
 import styles from './index.module.css';
 
 interface ButtonIconComponentProps {
   text: string;
   onClick: () => void;
+  type: string;
 }
 
-const ButtonIcon: React.FC<ButtonIconComponentProps> = ({ text, onClick }) => {
+const ButtonIcon: React.FC<ButtonIconComponentProps> = ({ text, onClick, type }) => {
   return (
     <button type="button" className={styles.button} id="button-icon" onClick={onClick}>
-      <EditIcon className={styles.icon} />
+      {
+        type === "Edit"? 
+        <EditIcon className={styles.icon} /> 
+        : 
+        <SearchIcon className={styles.icon} />
+      }
+      
       {text}
     </button>
   );
